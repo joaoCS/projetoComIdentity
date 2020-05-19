50076601d0398c061c510434a6a84bdf745895cf
JoaoCS <joaoantoniogba025@gmail.com>
2020-05-19 17:32:05 -0300

    Commit inicial. Foi adicionado um dbset para Post e foi criada a classe post e a migracão para Post.

---
+++ MyTestApp/Controllers/AccountController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyTestApp.Models;
+
+namespace MyTestApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<AppUser> UserMgr { get; }
+        private SignInManager<AppUser> SignInMgr { get; }
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            UserMgr = userManager;
+            SignInMgr = signInManager;
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await SignInMgr.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+        public async Task<IActionResult> Login()
+        {
+            var result = await SignInMgr.PasswordSignInAsync("TestUser", "Test123!", false, false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Result = "result is: " + result.ToString();
+            }
+            return View();
+        }
+
+
+        public async Task<IActionResult> Register()
+        {
+            try
+            {
+                ViewBag.Message = "User already registered";
+                AppUser user = await UserMgr.FindByNameAsync("TestUser");
+
+                if (user == null )
+                {
+                    user = new AppUser();
+
+                    user.UserName = "TestUser";
+                    user.Email = "TestUser@test.com";
+                    user.FirstName = "John";
+                    user.LastName = "Doe";
+
+                    IdentityResult result = await UserMgr.CreateAsync(user, "Test123!");
+                    ViewBag.Message = "User was created";
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ MyTestApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyTestApp.Models;
+
+namespace MyTestApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IdentityAppContext _context;
+
+
+        public HomeController(IdentityAppContext context, ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Posts.ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
